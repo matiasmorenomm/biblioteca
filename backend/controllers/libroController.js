d17c5564cce3503df5436fb8ab0e9ed4f04e1c5d
Matias <maatiigmm@gmail.com>
2020-08-08 17:28:56 -0400

    libroController

--- backend/controllers/libroController.js
+++ backend/controllers/libroController.js
@@ -1,3 +1,87 @@
 'use strict'
 
 var Libro = require('../models/libroModel');
+
+function guardar(req, res) {
+  let libro = new Libro();
+
+  libro.titulo = req.body.titulo,
+    libro.codigo = req.body.codigo,
+    libro.autor = req.body.autor,
+    libro.idioma = req.body.idioma
+
+  Libro.findOne({
+    codigo: libro.codigo
+  }).exec((err, lib) => {
+    if (lib) {
+      return res.status(400).send({
+        mensaje: 'Ya se encuentra este codigo registrado'
+      });
+    } else {
+      libro.save((err, libroStore) => {
+        if (err || !libroStore) {
+          return res.status(400).send({
+            mensaje: 'El libro no se pudo registrar'
+          })
+        } else {
+          return res.status(200).send({
+            mensaje: 'El libro se registro de froma exitosa',
+            libro: libroStore
+          })
+        }
+      })
+    }
+  })
+}
+
+function libro(req, res) {
+  let codigo = req.params.codigo
+
+  Libro.findOne({
+    codigo: codigo
+  }).exec((err, lib) => {
+    if (err || !lib) {
+      return res.status(400).send({
+        mensaje: 'No existe un libro con este codigo'
+      })
+    } else {
+      return res.status(200).send({
+        libro: libro
+      })
+    }
+  })
+}
+
+function eliminar(req, res) {
+  let codigo = req.params.codigo
+
+  Libro.findOne({
+    codigo: codigo
+  }).exec((err, lib) => {
+    if (err || !lib) {
+      return res.status(400).send({
+        mensaje: 'No existe un libro con este codigo'
+      })
+    } else {
+      Libro.deleteOne({
+        codigo: codigo
+      }, (err, LibroDeleted) => {
+        if (err || !LibroDeleted) {
+          return res.status(400).send({
+            mensaje: 'No se ha podido eliminar el libro'
+          })
+        } else {
+          return res.status(200).send({
+            mensaje: 'El libro se elimino de forma exitosa'
+          })
+        }
+      })
+    }
+  })
+}
+
+module.exports = {
+  guardar,
+  libro,
+  eliminar
+}
